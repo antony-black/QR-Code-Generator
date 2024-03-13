63a32985817f4ed60afacca08616c1d94ede33fe
Anton Knysh <anton@profairgames.com>
2024-03-13 17:08:21 +0200

    crated the project

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import "./index.scss";
+
+export default function App() {
+  return <div className="app">
+   
+  </div>;
+}
